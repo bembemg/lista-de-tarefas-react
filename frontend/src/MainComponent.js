75c276ab5779834638701869e5aba4957a205415
Gabriel Bembem <92057057+bembemg@users.noreply.github.com>
2024-11-04 22:07:11 -0300

    drag and drop adicionado

--- frontend/src/MainComponent.js
+++ frontend/src/MainComponent.js
@@ -1,8 +1,19 @@
 import React, { useCallback, useRef, useState } from 'react';
 import dayjs from 'dayjs';
+import { ReactSortable } from "react-sortablejs";
 import './styles/App.css';
 
+
 function MainComponent() {
+    // Variáveis para criação de itens
+    const [task, setTask] = useState([]);
+    const [taskName, setTaskName] = useState('');
+    const [expense, setExpense] = useState('');
+    const [date, setDate] = useState('');
+    // Variável para edição de item
+    const [edit, setEdit] = useState(null);
+    // Variável para exclusão de item
+    const [itemToDelete, setItemToDelete] = useState(null);
 
     // Abrir dialog para adicionar item a lista
     const modal = useRef();
@@ -20,16 +31,15 @@ function MainComponent() {
             modal.current.close();
         }
     }, []);
-    // Abrir dialog para editar item da lista
-
-    // Variáveis para criação de itens
-    const [task, setTask] = useState([]);
-    const [taskName, setTaskName] = useState('');
-    const [expense, setExpense] = useState('');
-    const [date, setDate] = useState('');
-    // Variável para edição de item
-    const [edit, setEdit] = useState(null);
-
+    
+    const remove = useRef();
+    const closeRemoveModal = useCallback(() => {
+        if (remove.current) {
+            setItemToDelete(null);
+            remove.current.close();
+        };
+    }, []);
+        
     // Adicionar Task
     const addTask = useCallback(() => {
         if (taskName.trim() !== '' && expense.trim() !== '' && date.trim() !== '') {
@@ -55,15 +65,17 @@ function MainComponent() {
             setDate('');
             setEdit(null);
             closeModal();
+        } else {
+            alert('Preencha todos os campos antes de adicionar.');
         };
     }, [taskName, expense, date, edit, closeModal, task]);
-
+    
     // ----------------------------------------
-
+    
     const editExistingTask = useCallback((index) => {
         const tarefa = task[index];
         setTaskName(tarefa.name);
-
+        
         // Verifica se a tarefa.expense é um número
         const expense = typeof tarefa.cost === 'number' ? tarefa.cost : 0;
         const formattedExpense = expense.toLocaleString('pt-br', {
@@ -75,7 +87,7 @@ function MainComponent() {
         const [dia, mes, ano] = tarefa.limitDate.split('/');
         const formattedDate = dayjs(`${ano}-${mes}-${dia}`).format('YYYY-MM-DD');
         setDate(formattedDate);
-
+        
         setEdit(index);
         modal.current.showModal();
     }, [task]);  
@@ -92,13 +104,13 @@ function MainComponent() {
         let value = event.target.value;
         value = value.replace(/\D/g, ""); // Remove caracteres não numéricos.
         value = (value / 100).toFixed(2).replace(".", ","); // Converte para números com centavos.
-
+        
         // Adiciona pontos a cada 3 caracteres após a vírgula.
         value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
+        
         setExpense(value);
     };
-
+    
     // ----------------------------------------
     
     // Limita a quantia de caracteres no input de data limite
@@ -110,7 +122,19 @@ function MainComponent() {
     };
 
     // ----------------------------------------
-    
+
+    // Confirma se o usuário deseja excluir o item
+    const deleteTask = useCallback((index) => {
+        if (remove.current && itemToDelete !== null) {
+            const updatedTasks = [...task];
+            updatedTasks.splice(itemToDelete, 1);
+            setTask(updatedTasks);
+            closeRemoveModal();
+        };
+    }, [closeRemoveModal, task, itemToDelete]);
+
+    // ----------------------------------------
+
     return (
         <main className="container">
 
@@ -127,79 +151,85 @@ function MainComponent() {
                     <span>Data Limite</span>
                 </header>
                 
-                {/* Lista ordenada -> */}
-                <ol>
-                    {/* Exemplo de item na lista
-                    <li>
-                        <div className="task">
-                            <strong>Desenvolver tal coisa</strong>
-                        </div>
+        {/* Lista ordenada -> */}
 
-                        <span className="task-expense">
-                            <small>R$</small>
-                            400,00
-                        </span>
+        <ReactSortable 
+            tag="ol"
+            list={task}
+            setList={setTask}
+            animation={200}
+            delay={150} // Tempo em milissegundos para segurar antes de arrastar
+            delayOnTouchOnly={true} // Aplica o delay apenas em dispositivos touch
+            touchStartThreshold={5} // Tolerância de movimento antes de iniciar o arrasto
+        >
 
-                        <div className="date">
-                            <span>08/11/2024</span>
-                        </div>
+        {task.map((tarefa, index) => (
+            <li key={index}>
+                <div className="task"><strong>{tarefa.name}</strong></div>
 
-                        <div className="buttons">
-                            <img src={`${process.env.PUBLIC_URL}/assets/edit.svg`} className="edit-btn" alt="editar" />
-                            <img src={`${process.env.PUBLIC_URL}/assets/remove.svg`} className="remove-btn" alt="remover" />
-                        </div>
-                    </li> */}
+                <span className="task-expense">{formatBRL(tarefa.cost)}</span>
 
-                    {/* Estrutura dos itens da lista -> */}
-                    {task.map((tarefa, index) => (
-                        <li key={index}>
-                            <div className="task"><strong>{tarefa.name}</strong></div>
+                <div className="date"><span>{tarefa.limitDate}</span></div>
 
-                            <span className="task-expense">{formatBRL(tarefa.cost)}</span>
-
-                            <div className="date"><span>{tarefa.limitDate}</span></div>
-
-                            <div className="buttons">
-                            <img src={`${process.env.PUBLIC_URL}/assets/edit.svg`} 
-                            className="edit-btn" alt="editar" 
-                            onClick={() => editExistingTask(index)} />
-
-                            <img src={`${process.env.PUBLIC_URL}/assets/remove.svg`} className="remove-btn" alt="remover" />
-                            </div>
-                        </li>
-                    ))}
-                </ol>
-
-                {/* ------------------------------ */}
-
-                {/* Botão de incluir item a lista de tarefas -> */}
-                <button id="btn-include" onClick={showModal}>Incluir</button>
-                <dialog id="add" ref={modal}>
-
+                <div className="buttons">
+                    <img src={`${process.env.PUBLIC_URL}/assets/edit.svg`} 
+                        className="edit-btn" 
+                        alt="editar" 
+                        onClick={() => editExistingTask(index)} />
                     <img src={`${process.env.PUBLIC_URL}/assets/remove.svg`} 
-                    className="remove-btn" onClick={closeModal} alt="cancelar"/>
+                        className="remove-btn" 
+                        alt="remover"
+                        onClick={() => {
+                            setItemToDelete(index);
+                            remove.current?.showModal();
+                        }} 
+                    />
+                </div>
+            </li>
+        ))}
+        </ReactSortable>
 
-                    <input type="text" placeholder="Nome da Tarefa" className="add-task" 
-                    value={taskName}
-                    onChange={(event) =>
-                        setTaskName(event.target.value)
-                    }/>
+            {/* ------------------------------ */}
 
-                    <input type="text" placeholder="R$ 0,00" className="add-expense"
+            {/* Botão de incluir item a lista de tarefas -> */}
+            <button id="btn-include" onClick={showModal}>Incluir</button>
+
+            {/* dialog para adicionar novo item a lista -> */}
+            <dialog id="add" ref={modal}>
+
+                <img src={`${process.env.PUBLIC_URL}/assets/remove.svg`} 
+                className="remove-btn" onClick={closeModal} alt="cancelar"/>
+
+                <input type="text" placeholder="Nome da Tarefa" className="add-task" 
+                value={taskName}
+                onChange={(event) =>
+                    setTaskName(event.target.value)
+                }/>
+
+                <input type="text" placeholder="R$ 0,00" className="add-expense"
                     value={expense}
                     onChange={handleExpenseChange}/>
 
-                    <input type="date" className="add-date" maxLength="10"
-                    value={date}
-                    onChange={handleLimitDateChange}/>
+                <input type="date" className="add-date" maxLength="10"
+                value={date}
+                onChange={handleLimitDateChange}/>
 
-                    <button id="btn-add" onClick={addTask}>{edit !== null ? "Salvar" : "Adicionar"}</button>
-                </dialog>
+                <button id="btn-add" onClick={addTask}>{edit !== null ? "Salvar" : "Adicionar"}</button>
+            </dialog>
 
-                {/* ------------------------------ */}
+            {/* ------------------------------ */}
+
+            {/* dialog de confirmação de exclusão de item */}
+            <dialog id="remove" ref={remove}>
+                <p>Tem certeza que deseja excluir este item?</p>
+                <img src={`${process.env.PUBLIC_URL}/assets/confirm.svg`} alt="confirmar"
+                    onClick={deleteTask}/>
+                <img src={`${process.env.PUBLIC_URL}/assets/remove.svg`} alt="cancelar" 
+                    onClick={closeRemoveModal}/>
+            </dialog>
             </section>
-        </main>
+    </main>
     );
-}
+};
 
 export default MainComponent;
